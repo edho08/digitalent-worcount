e97a5698d5693bbaf1371bf178f36f5f179eb95f
edho <edh08@gmail.com>
2019-06-26 22:42:47 +0700

    init

---
+++ mapper.py
@@ -0,0 +1,10 @@
+#!/home/ubuntu/anaconda3/bin/python
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    words = line.split()
+    if len(line) > 1:
+        for word in words:
+            print ('%s\t%d' % (word, 1))
